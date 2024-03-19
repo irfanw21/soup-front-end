b07cecc7cc1c44d7b8db1c7f6aba40af543d70e7
irfanw21 <irfan.wijaya123@gmail.com>
2024-03-19 20:29:29 +0700

    Latihan Modify hompage dan operasi vite

---
+++ Front-end-code/src/Pages/homepage/index.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import './index.css'
+
+const Homepage = () => {
+    const [state, setState] = useState(0)
+
+    const handleClick = () => {
+        setState(state + 1)
+    }
+
+    const handleKurang = () => {
+        if (state > 0){
+            setState(state - 1)
+        }
+    }
+    return (
+        <div className = "homepage"> 
+        <h1>Homepage</h1>
+        <h2>{state}</h2>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={handleKurang}>Kurang</button>
+        </div>
+    );
+}
+
+export default Homepage
